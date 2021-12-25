49595c3c8b160e8d7fbc24b7d61ae1702b4cec31
Colin McFadden <cmcfadden@gmail.com>
2021-12-24 22:33:43 -0600

    add an explicit return and background teh jobs

--- climatize.sh
+++ climatize.sh
@@ -5,4 +5,5 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
-python3 cli.py climatize $USERNAME $PASSWORD north_america $VIN
+python3 cli.py climatize $USERNAME $PASSWORD north_america $VIN &
+exit 1
--- lock.sh
+++ lock.sh
@@ -5,4 +5,5 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
-python3 cli.py lock $USERNAME $PASSWORD north_america $VIN
+python3 cli.py lock $USERNAME $PASSWORD north_america $VIN &
+exit 1
